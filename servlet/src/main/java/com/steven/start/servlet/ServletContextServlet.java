a5ea543ee348f88f4c78c2ae278a6115e81f555d
cxq <1249560518@qq.com>
2020-12-01 17:06:18 +0800

    全局参数

---
+++ servlet/src/main/java/com/steven/start/servlet/ServletContextServlet.java
@@ -0,0 +1,26 @@
+package com.steven.start.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author CXQ
+ * @version 1.0
+ */
+@WebServlet("/servlet/servlet_context")
+public class ServletContextServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println(super.getServletContext().getInitParameter("loc"));
+        System.out.println(super.getServletContext().getInitParameter("type"));
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.doGet(req, resp);
+    }
+}
