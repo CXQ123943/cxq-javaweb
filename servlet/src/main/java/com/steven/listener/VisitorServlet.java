f2490c41950295bde516dfc0d6564baae14f5abe
cxq <1249560518@qq.com>
2020-12-12 15:51:04 +0800

    servlet监视器：模拟统计用户登录人数案例

---
+++ servlet/src/main/java/com/steven/listener/VisitorServlet.java
@@ -0,0 +1,49 @@
+package com.steven.listener;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+/**
+ * @author CXQ
+ * @version 1.0
+ */
+@WebServlet("/api/visitor")
+public class VisitorServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String meta = req.getParameter("meta");
+
+        if (Meta.LOGIN.equals(meta)) {
+
+            // 假设登录成功
+            String name = req.getParameter("name");
+            HttpSession session;
+            synchronized (session = req.getSession()) {
+                session.setAttribute("name", req.getParameter("name"));
+            }
+
+            ServletContext application;
+            synchronized (application = req.getServletContext()) {
+                int visitorCount = (int) application.getAttribute("visitorCount");
+                application.setAttribute("visitorCount", ++visitorCount);
+                System.out.println("当前第" + visitorCount + "人登录了您的网站！");
+            }
+            resp.getWriter().print("{\"name\":" + name + "}");
+        }
+    }
+
+    interface Meta {
+        String LOGIN = "login";
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.doGet(req, resp);
+    }
+}
